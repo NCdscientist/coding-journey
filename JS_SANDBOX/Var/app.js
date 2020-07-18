c12475dde74dea3ef4226f1a74b6a67b2ee0d7ea
NCdscientist <naveenchalla666@gmail.com>
2020-07-18 16:55:34 -0400

    'varexercise'

---
+++ JS_SANDBOX/Var/app.js
@@ -0,0 +1,37 @@
+// var name = 'Naveen';
+// console.log(name);
+
+// name = 'Naveen 2'
+// console.log(name)
+
+// //Init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello Naveen';
+// console.log(greeting);
+
+// // letters, numbers,_,$
+// // Cannot start with a number 
+
+// //Multi word vars name
+// var firstName = 'N'; //Camel case
+// var first_name = 'Sara';//Underscore
+// var FirstName = 'Tom'; //Pascalcase 
+// var firstname;
+
+//Let
+// let name = 'Naveen';
+// console.log(name);
+
+//CONST
+const person ={
+  name: 'Navi',
+  age: 27
+}
+person.name ='Sara';
+ person.age =32;
+
+// console.log(person);
+ const numbers=  [1,2,3,4,5];
+ numbers.push(6);
+ console.log(numbers);
